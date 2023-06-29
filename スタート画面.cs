83ef7bdce113dda7db75ded4c2b4c9ba498789da
tomoyukiharada <tomotyuki.h.1435@gmail.com>
2023-06-29 21:59:08 +0900

    :recycle: refactor

--- スタート画面.cs
+++ スタート画面.cs
@@ -21,9 +21,8 @@ namespace unilab2023
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ステージ選択画面 form = new ステージ選択画面();
-            form.Show();
-            //this.Close();  //画面遷移すると自動的に閉じる設定にするならコメントイン、ただし現設定だとプロジェクトが終了してしまう
+            ステージ選択画面 form2 = new ステージ選択画面();
+            form2.Show();
         }
 
         private void スタート画面_Load(object sender, EventArgs e)
