c7bb9a36e1ff18e73518bf23f7a82b639df4ad81
GentaShinya <113504673+GentaShinya@users.noreply.github.com>
2023-06-30 19:22:04 +0900

    Add files via upload
    画面の大きさとレイアウト変更

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,6 +48,8 @@
             this.label6 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -59,11 +61,11 @@
             // 
             this.listBox1.AllowDrop = true;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 18;
-            this.listBox1.Location = new System.Drawing.Point(91, 108);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(73, 112);
             this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(146, 310);
+            this.listBox1.Size = new System.Drawing.Size(118, 259);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
@@ -73,11 +75,11 @@
             // 
             this.listBox3.AllowDrop = true;
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 18;
-            this.listBox3.Location = new System.Drawing.Point(320, 108);
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(256, 112);
             this.listBox3.Margin = new System.Windows.Forms.Padding(2);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(146, 310);
+            this.listBox3.Size = new System.Drawing.Size(118, 259);
             this.listBox3.TabIndex = 1;
             this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             this.listBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
@@ -87,11 +89,11 @@
             // 
             this.listBox4.AllowDrop = true;
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 18;
-            this.listBox4.Location = new System.Drawing.Point(602, 108);
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(482, 112);
             this.listBox4.Margin = new System.Windows.Forms.Padding(2);
             this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(146, 310);
+            this.listBox4.Size = new System.Drawing.Size(118, 259);
             this.listBox4.TabIndex = 2;
             this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
             this.listBox4.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
@@ -99,44 +101,44 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(755, 108);
+            this.pictureBox2.Location = new System.Drawing.Point(604, 90);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(402, 346);
+            this.pictureBox2.Size = new System.Drawing.Size(322, 288);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(121, 434);
+            this.button2.Location = new System.Drawing.Point(97, 391);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(71, 32);
+            this.button2.Size = new System.Drawing.Size(70, 30);
             this.button2.TabIndex = 4;
-            this.button2.Text = "delete";
+            this.button2.Text = "リセット";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(338, 430);
+            this.button3.Location = new System.Drawing.Point(275, 391);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 36);
+            this.button3.Size = new System.Drawing.Size(70, 30);
             this.button3.TabIndex = 5;
-            this.button3.Text = "delete";
+            this.button3.Text = "リセット";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(619, 439);
+            this.button4.Location = new System.Drawing.Point(505, 391);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(70, 30);
             this.button4.TabIndex = 6;
-            this.button4.Text = "delete";
+            this.button4.Text = "リセット";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -149,7 +151,7 @@
             // 
             this.listBox2.AllowDrop = true;
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 18;
+            this.listBox2.ItemHeight = 15;
             this.listBox2.Items.AddRange(new object[] {
             "↑",
             "→",
@@ -157,34 +159,35 @@
             "←",
             "for (1)",
             "endfor"});
-            this.listBox2.Location = new System.Drawing.Point(175, 478);
+            this.listBox2.Location = new System.Drawing.Point(135, 506);
             this.listBox2.Margin = new System.Windows.Forms.Padding(2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(146, 166);
+            this.listBox2.Size = new System.Drawing.Size(118, 139);
             this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // listBox5
             // 
             this.listBox5.AllowDrop = true;
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 18;
+            this.listBox5.ItemHeight = 15;
             this.listBox5.Items.AddRange(new object[] {
             "A",
             "B"});
-            this.listBox5.Location = new System.Drawing.Point(395, 506);
+            this.listBox5.Location = new System.Drawing.Point(348, 506);
             this.listBox5.Margin = new System.Windows.Forms.Padding(2);
             this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(146, 112);
+            this.listBox5.Size = new System.Drawing.Size(118, 94);
             this.listBox5.TabIndex = 8;
             this.listBox5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox3.Location = new System.Drawing.Point(78, 703);
+            this.pictureBox3.Location = new System.Drawing.Point(1697, 123);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(786, 73);
+            this.pictureBox3.Size = new System.Drawing.Size(65, 61);
             this.pictureBox3.TabIndex = 9;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
@@ -192,12 +195,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(662, 593);
+            this.button1.Location = new System.Drawing.Point(1036, 861);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 50);
+            this.button1.Size = new System.Drawing.Size(120, 40);
             this.button1.TabIndex = 10;
-            this.button1.Text = "start";
+            this.button1.Text = "出発！";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -208,74 +211,76 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(1098, 625);
+            this.groupBox1.Location = new System.Drawing.Point(275, 722);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(238, 175);
+            this.groupBox1.Size = new System.Drawing.Size(190, 146);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(128, 103);
+            this.label5.Location = new System.Drawing.Point(102, 86);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(17, 18);
+            this.label5.Size = new System.Drawing.Size(15, 15);
             this.label5.TabIndex = 16;
             this.label5.Text = "0";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 103);
+            this.label4.Location = new System.Drawing.Point(23, 86);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 18);
+            this.label4.Size = new System.Drawing.Size(65, 15);
             this.label4.TabIndex = 15;
             this.label4.Text = "ミス回数：";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(128, 49);
+            this.label3.Location = new System.Drawing.Point(102, 41);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 18);
+            this.label3.Size = new System.Drawing.Size(15, 15);
             this.label3.TabIndex = 14;
             this.label3.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 49);
+            this.label2.Location = new System.Drawing.Point(23, 41);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.Size = new System.Drawing.Size(75, 15);
             this.label2.TabIndex = 13;
             this.label2.Text = "試行回数：";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 18F);
-            this.label6.Location = new System.Drawing.Point(748, 516);
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.ForeColor = System.Drawing.Color.Tomato;
+            this.label6.Location = new System.Drawing.Point(1091, 356);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 48);
+            this.label6.Size = new System.Drawing.Size(238, 106);
             this.label6.TabIndex = 12;
             this.label6.Text = "ミス！";
             this.label6.Visible = false;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(841, 593);
+            this.button6.Location = new System.Drawing.Point(1241, 861);
             this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(148, 46);
+            this.button6.Size = new System.Drawing.Size(120, 40);
             this.button6.TabIndex = 14;
             this.button6.Text = "リトライ";
             this.button6.UseVisualStyleBackColor = true;
@@ -283,22 +288,49 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(755, 108);
+            this.pictureBox1.Location = new System.Drawing.Point(604, 90);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(402, 346);
+            this.pictureBox1.Size = new System.Drawing.Size(322, 288);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(82, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 15);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "A";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(256, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 15);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.Text = "B";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1418, 803);
+            this.ClientSize = new System.Drawing.Size(1829, 912);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox3);
@@ -313,6 +345,8 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "ゲーム画面";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -346,6 +380,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
