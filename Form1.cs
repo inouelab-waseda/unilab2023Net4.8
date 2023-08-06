0b7f3a27c66866f53c9144f2ddf292fe63879f53
KeihakuOh <w1123527817@akane.waseda.jp>
2023-08-06 18:31:13 +0900

    コメントアウト

--- Form1.cs
+++ Form1.cs
@@ -173,7 +173,7 @@ namespace unilab2023
         public void Form1_Load(object sender, EventArgs e)
         {
             button5.Visible = false;
-            _stageName = "stage3-2";
+            //_stageName = "stage3-2";
 
             Global.map = CreateStage(stageName); //ステージ作成
 
